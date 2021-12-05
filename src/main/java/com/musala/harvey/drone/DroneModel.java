f97f2e15c373da18091051c97eba5181c07edd44
harvey.imama <harveyimama@yahoo.com>
2021-12-05 18:56:43 +0100

    first commit

--- src/main/java/com/musala/harvey/drone/DroneModel.java
+++ src/main/java/com/musala/harvey/drone/DroneModel.java
@@ -3,4 +3,18 @@ package com.musala.harvey.drone;
 public enum DroneModel {
     Lightweight("Lightweight"), Middleweight("Middleweight"), 
     Cruiserweight("Cruiserweight"), Heavyweight("Heavyweight") ;
+
+    private String val;
+    private DroneModel(String val)
+    {
+        this.val = val;
+    }
+    public String getVal() {
+        return val;
+    }
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+    
 }
--- src/main/java/com/musala/harvey/drone/DroneState.java
+++ src/main/java/com/musala/harvey/drone/DroneState.java
@@ -1,8 +1,25 @@
 package com.musala.harvey.drone;
 
 public enum DroneState {
+
     IDLE("IDLE"), LOADING("LOADING"), LOADED("LOADED"), DELIVERING("DELIVERING"),
      DELIVERED("DELIVERED"), RETURNING("RETURNING");
+
+     private String val;
+
+     DroneState(String val)
+     {
+            this.val = val;
+     }
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
     
+     
     
 }
