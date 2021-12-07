d6951f569984cfd3130b414f8513453b48583994
harvey.imama <harveyimama@yahoo.com>
2021-12-07 05:13:32 +0100

    test schedulers

---
+++ src/main/java/com/musala/harvey/drone/Env.java
@@ -0,0 +1,9 @@
+package com.musala.harvey.drone;
+
+public class Env {
+
+    public static final long BATTERY_LEVELS_CHECK_TIME = 10;
+    public static  int  DRONE_DEFAULT_CAPACITY;
+    public static int NO_OF_SEEDED_DRONES;
+    
+}
